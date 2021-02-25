2d64164ab440518c36e1cdfe81d1de1c7f909e11
Øyvind gjesdal <oyvind@localhost.localdomain>
2021-02-25 22:36:50 +0100

    try to change max wait time

--- jena-fuseki2/jena-fuseki-webapp/src/test/java/org/apache/jena/fuseki/TestAdmin.java
+++ jena-fuseki2/jena-fuseki-webapp/src/test/java/org/apache/jena/fuseki/TestAdmin.java
@@ -481,10 +481,9 @@ public class TestAdmin extends AbstractFusekiTest {
     @Test public void task_6() {
         String x1 = execSleepTask(null, 1000);
         String x2 = execSleepTask(null, 1000);
-        String x3 = execSleepTask(null, 1000);
         List<String> running = runningTasks();
         assertTrue(running.size()>1);
-        waitForTasksToFinish(1000, 2000);
+        waitForTasksToFinish(1000, 3000);
     }
 
     @Test public void task_7() {
