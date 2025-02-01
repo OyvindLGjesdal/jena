753fb6c1a9c3011a22461abb5f22a009d67348e3
Øyvind Gjesdal <oyvind.gjesdal@uib.no>
2025-02-01 03:30:26 +0100

    silence deprecations, specify modern api to avoid legacy warn

--- jena-fuseki2/jena-fuseki-ui/vite.config.js
+++ jena-fuseki2/jena-fuseki-ui/vite.config.js
@@ -23,6 +23,14 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern",
+        silenceDeprecations: ["import", "global-builtin","color-functions","mixed-decls"],
+      },
+    },
+  },
   base: './',
   plugins: [
     vue(),
