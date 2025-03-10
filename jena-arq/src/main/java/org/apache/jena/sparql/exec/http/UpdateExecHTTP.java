c847907a511c6b5aaa21140b3deb6019fef70cad
Øyvind Gjesdal <oyvind.gjesdal@uib.no>
2025-03-10 23:02:16 +0100

    comment out logUpdate to not use request before response (not used in function?)

--- jena-arq/src/main/java/org/apache/jena/sparql/exec/http/UpdateExecHTTP.java
+++ jena-arq/src/main/java/org/apache/jena/sparql/exec/http/UpdateExecHTTP.java
@@ -139,7 +139,7 @@ public class UpdateExecHTTP implements UpdateExec {
         HttpRequest.Builder builder = HttpLib.requestBuilder(requestURL, httpHeaders, timeout, timeoutUnit);
         builder = contentTypeHeader(builder, contentType);
         HttpRequest request = builder.POST(body).build();
-        logUpdate(updateString, request);
+        //logUpdate(updateString, request);
         HttpResponse<InputStream> response = HttpLib.execute(httpClient, request);
         return HttpLib.handleResponseRtnString(response, this::setRetainedConnection);
     }
